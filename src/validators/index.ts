84bfbd88de7bab9cd73179de70a3deed5e3e8dca
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2022-04-16 21:56:13 +0300

    Validator error array bug is fixed

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express";
 export default function (req: Request, res: Response, next: Function) {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		console.error(errors.array().at(0)?.msg);
+		console.error(errors.array()[0]?.msg);
 		return res.status(422).json({ errors: errors.array()[0].msg });
 	}
 	next();
